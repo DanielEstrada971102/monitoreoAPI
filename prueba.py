ceabb03b8af14cd2e18d9e13e51eebd7448bab9c
Daniel Estrada <daniel.estrada.acevedo@cern.ch>
2023-06-10 03:46:17 -0500

    fix DB uri

--- prueba.py
+++ prueba.py
@@ -2,7 +2,7 @@ import requests
 
 dato = {"lugar":"Girardota", "temperatura": 25, "humedad": 67}
 
-res = requests.post("http://localhost:8000/monitoreo", json=dato)
+res = requests.post("mongodb+srv://DanielEstrada:danielestrada@iot-example-database.kp5zpry.mongodb.net/?retryWrites=true&w=majority", json=dato)
 
 print(res.text)
 
